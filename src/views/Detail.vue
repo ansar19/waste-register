ed330ca7d074085237b4ca847c4201aee3331c7e
ansar19 <mos-kz@ya.ru>
2020-05-31 15:45:03 +0600

    qr-code integration to details

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,29 +2,58 @@
   <div>
     <Loader v-if="loading" />
     <div v-else-if="record">
-
       <nav aria-label="breadcrumb">
-        <div class="">
+        <div class>
           <router-link to="/history" class="breadcrumb-item ml-2">{{'Menu_History'|localize}}</router-link>
-          <a @click.prevent class="breadcrumb-item">{{ record.type === 'income' ? 'Income' : 'Outcome' | localize }}</a>
+          <a
+            @click.prevent
+            class="breadcrumb-item"
+          >{{ record.type === 'income' ? 'Income' : 'Outcome' | localize }}</a>
         </div>
       </nav>
 
       <div class="row">
-        <div class="col s12 m6">
-          <div class="card" :class="{
+        <div class="col-sm-2">
+          <div class="card">
+            <div class="card-body dark-grey-text">
+              <div class="d-flex justify-center">
+                <qr-code :text="record.id" size="116" color="#17c671" error-level="H"></qr-code>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-10">
+          <div
+            class="card"
+            :class="{
             'yellow': record.type === 'outcome',
             'alert-info': record.type === 'income'
-          }">
+          }"
+          >
             <div class="card-body dark-grey-text">
               <small>{{record.date | date('datetime')}}</small>
-              <p>{{'Category'|localize}}: <small>{{record.categoryName}}</small></p>
-              <p>{{'Amount'|localize}}: <small>{{record.amount | currency}}</small></p>
-              <p>{{'Description'|localize}}: <small>{{record.description}}</small></p>
-              <p>{{'Recycling_Type'|localize}}: <small>{{record.recyclingType}}</small></p>
-              <p>{{'Precaution_Title'|localize}}: <small>{{record.precaution}}</small></p>
+              <p>
+                {{'Category'|localize}}:
+                <small>{{record.categoryName}}</small>
+              </p>
+              <p>
+                {{'Amount'|localize}}:
+                <small>{{record.amount | currency}}</small>
+              </p>
+              <p>
+                {{'Description'|localize}}:
+                <small>{{record.description}}</small>
+              </p>
+              <p>
+                {{'Recycling_Type'|localize}}:
+                <small>{{record.recyclingType}}</small>
+              </p>
+              <p>
+                {{'Precaution_Title'|localize}}:
+                <small>{{record.precaution}}</small>
+              </p>
             </div>
-
           </div>
         </div>
       </div>
@@ -34,6 +63,8 @@
 </template>
 
 <script>
+import VueQRCodeComponent from 'vue-qrcode-component'
+
 export default {
   name: 'detail',
   metaInfo() {
@@ -63,6 +94,11 @@ export default {
     }
 
     this.loading = false
+  },
+  methods: {},
+  computed: {},
+  components: {
+    'qr-code': VueQRCodeComponent
   }
 }
 </script>
