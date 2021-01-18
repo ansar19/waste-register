9c641b22a6c70a8abf683c12d9c988516679e649
ansar19 <mos-kz@ya.ru>
2021-01-18 23:07:15 +0600

    ROP create in record

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -44,6 +44,47 @@
           <option v-for="c in categories" :key="c.id" :value="c.id">{{c.title}}</option>
         </select>
       </div>
+      
+      <!-- РОП -->
+       <fieldset>
+         <div class="form-check custom-control custom-radio d-block my-2">
+           <input class="form-check-input custom-control-input" type="radio" v-bind:value="false" v-model="isRop"
+             name="flexRadioDefault" id="flexRadioDefault1">
+           <label class="form-check-label custom-control-label" for="flexRadioDefault1">
+             Отход не относится к группе РОП
+           </label>
+         </div>
+
+
+         <div class="form-check custom-control custom-radio d-block my-2">
+           <input class="form-check-input custom-control-input" type="radio" v-bind:value="true" v-model="isRop"
+             name="flexRadioDefault" id="flexRadioDefault2">
+           <label class="form-check-label custom-control-label" for="flexRadioDefault2">
+             РОП
+           </label>
+         </div>
+
+      <template v-if="isRop == true">
+        <div class="mb-3">
+          <div>
+            <label class="form-label">Выберите из перечня продукции (товаров), на которую (которые) распространяются расширенные обязательства производителей (импортеров)</label>
+            <v-select :options="ropOptions" label="productType" v-model="selectedRop" :selectable="option => !option.hasOwnProperty('group')" class="style-chooser"> 
+            <template #option="{ group, productType }">
+              <div v-if="group" class="group">
+                {{ group }}
+              </div>
+              {{ productType }}
+            </template>
+          </v-select>
+          <span>{{selectedRop.codeTNVED}} - {{selectedRop.productType}}</span> 
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        Отход не относится к группе РОП
+      </template>
+      </fieldset>
+      
 
       <!-- Перевозчик -->
 
@@ -121,6 +162,7 @@
         {{'Create'|localize}}
         <i class="material-icons right">send</i>
       </button>
+
     </form>
   </div>
 </template>
@@ -162,6 +204,156 @@ export default {
     select3: null,
     sites: [],
     categories: [],
+    isRop: false,
+    selectedRop: [],
+    ropOptions: [
+      {
+        group:
+          "Средства наземного транспорта, кроме железнодорожного или трамвайного подвижного состава, и их части и принадлежности",
+        productType: null
+      },
+      {
+        codeTNVED: "8701 20 101",
+        productType:
+          "новые седельные тягачи, за исключением транспортных средств полной массы свыше 50 тонн"
+      },
+      {
+        codeTNVED: "8701 20 901",
+        productType:
+          "бывшие в эксплуатации седельные тягачи, за исключением транспортных средств полной массы свыше 50 тонн"
+      },
+      {
+        codeTNVED: "8702",
+        productType:
+          "моторные транспортные средства, предназначенные для перевозки 10 человек или более, включая водителя"
+      },
+      {
+        codeTNVED: "8703",
+        productType:
+          "автомобили легковые и прочие моторные транспортные средства, предназначенные главным образом для перевозки людей (кроме моторных транспортных средств товарной позиции 8702), включая грузопассажирские автомобили-фургоны и гоночные автомобили"
+      },
+      {
+        codeTNVED: "8704",
+        productType:
+          "моторные транспортные средства для перевозки грузов, за исключением транспортных средств полной массы свыше 50 тонн"
+      },
+      {
+        codeTNVED: "8705",
+        productType:
+          "моторные транспортные средства специального назначения (например, автомобили грузовые аварийные, автокраны, пожарные транспортные средства, автобетономешалки, автомобили для уборки дорог, поливомоечные автомобили, автомастерские, автомобили с рентгеновскими установками), кроме используемых для перевозки пассажиров или грузов, за исключением транспортных средств полной массы свыше 50 тонн"
+      },
+      {
+        codeTNVED: "8706 00 910 9",
+        productType: "прочие для транспортных средств товарной позиции 8703"
+      },
+      {
+        group: "Каучук, резина и изделия из них",
+        productType: null
+      },
+      {
+        codeTNVED: "4011 10 000",
+        productType:
+          "шины и покрышки пневматические резиновые новые, для легковых автомобилей (включая грузопассажирские автомобили-фургоны и спортивные автомобили)"
+      },
+      {
+        codeTNVED: "4011 20",
+        productType:
+          "шины и покрышки пневматические резиновые новые, для автобусов или моторных транспортных средств для перевозки грузов"
+      },
+      {
+        codeTNVED: "4011 30 000 0",
+        productType:
+          "шины и покрышки пневматические резиновые новые, для использования на воздушных судах"
+      },
+      {
+        codeTNVED: "4011 40 000 0",
+        productType:
+          "шины и покрышки пневматические резиновые новые, для мотоциклов"
+      },
+      {
+        codeTNVED: "4011 50 000",
+        productType:
+          "шины и покрышки пневматические резиновые новые, для велосипедов"
+      },
+      {
+        codeTNVED: "4011 70 000 0",
+        productType:
+          "шины и покрышки пневматические резиновые новые, для сельскохозяйственных или лесохозяйственных транспортных средств и машин"
+      },
+      {
+        codeTNVED: "4011 80 000 0",
+        productType:
+          "шины и покрышки пневматические резиновые новые, для транспортных средств и машин, используемых в строительстве, горном деле или промышленности"
+      },
+      {
+        codeTNVED: "4011 90 000 0",
+        productType: "прочие шины и покрышки пневматические резиновые новые"
+      },
+      {
+        codeTNVED: "4012 11 000 0",
+        productType:
+          "шины и покрышки пневматические резиновые, восстановленные или бывшие в употреблении; шины и покрышки массивные или полупневматические, шинные протекторы и ободные ленты, резиновые, шины и покрышки восстановленные, для легковых автомобилей (включая грузопассажирские автомобили-фургоны и спортивные автомобили)"
+      },
+      {
+        codeTNVED: "4012 12 000 0",
+        productType:
+          "шины и покрышки пневматические резиновые, восстановленные или бывшие в употреблении; шины и покрышки массивные или полупневматические, шинные протекторы и ободные ленты, резиновые, шины и покрышки восстановленные, для автобусов или моторных транспортных средств для перевозки грузов"
+      },
+      {
+        codeTNVED: "4012 13 000",
+        productType:
+          "шины и покрышки пневматические резиновые, восстановленные или бывшие в употреблении; шины и покрышки массивные или полупневматические, шинные протекторы и ободные ленты, резиновые, шины и покрышки восстановленные, для использования на воздушных судах"
+      },
+      {
+        codeTNVED: "4012 19",
+        productType:
+          "шины и покрышки пневматические резиновые, восстановленные или бывшие в употреблении; шины и покрышки массивные или полупневматические, шинные протекторы и ободные ленты, резиновые, шины и покрышки восстановленные, прочие"
+      },
+      {
+        codeTNVED: "4012 20 000",
+        productType:
+          "шины и покрышки пневматические резиновые, восстановленные или бывшие в употреблении; шины и покрышки массивные или полупневматические, шинные протекторы и ободные ленты, резиновые, шины и покрышки пневматические, бывшие в употреблении"
+      },
+      {
+        group: "Аккумуляторы электрические, включая сепараторы для них",
+        productType: null
+      },
+      {
+        codeTNVED: "8507 10",
+        productType:
+          "аккумуляторы электрические, включая сепараторы для них, прямоугольной (в том числе квадратной) или иной формы, свинцовые, используемые для запуска поршневых двигателей"
+      },
+      {
+        codeTNVED: "8507 20",
+        productType:
+          "аккумуляторы электрические, включая сепараторы для них, прямоугольной (в том числе квадратной) или иной формы, аккумуляторы свинцовые прочие"
+      },
+      {
+        codeTNVED: "8507 30",
+        productType:
+          "аккумуляторы электрические, включая сепараторы для них, прямоугольной (в том числе квадратной) или иной формы, никель-кадмиевые"
+      },
+      {
+        codeTNVED: "8507 40 000 0",
+        productType:
+          "аккумуляторы электрические, включая сепараторы для них, прямоугольной (в том числе квадратной) или иной формы, никель-железные"
+      },
+      {
+        codeTNVED: "8507 50 000 0",
+        productType:
+          "аккумуляторы электрические, включая сепараторы для них, прямоугольной (в том числе квадратной) или иной формы, гидридно-никелевые"
+      },
+      {
+        codeTNVED: "8507 60 000 0",
+        productType:
+          "аккумуляторы электрические, включая сепараторы для них, прямоугольной (в том числе квадратной) или иной формы, литий-ионные"
+      },
+      {
+        codeTNVED: "8507 80 000 0",
+        productType:
+          "аккумуляторы электрические, включая сепараторы для них, прямоугольной (в том числе квадратной) или иной формы, аккумуляторы прочие"
+      }
+    ],
     transporters: [],
     utilizators: [],
     site: null,
@@ -252,6 +444,8 @@ export default {
           await this.$store.dispatch('createRecord', {
             siteId: this.site,
             categoryId: this.category,
+            isRop: this.isRop,
+            selectedRop: this.selectedRop,
             transporterId: this.transporter,
             utilizatorId: this.utilizator,
             utilizationWay: this.utilizationWay,
@@ -304,3 +498,64 @@ export default {
   }
 }
 </script>
+
+<style>
+/* This fix the long text for closed vue-selects, fix long text in options during the search (they now break line). */
+.vs__selected-options {
+  flex-wrap: nowrap;
+  max-width: calc(100% - 41px);
+}
+
+.vs__selected {
+  display: block;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  overflow: hidden;
+}
+
+.vs__search {
+  position: absolute;
+}
+
+.vs--open .vs__search {
+  position: static;
+}
+
+.vs__dropdown-option {
+  white-space: normal;
+}
+
+.vs__dropdown-option--disabled {
+  background: #ededed;
+  color: #3b3b3b;
+}
+
+/* This fix the long text for closed vue-selects, fix long text in options during the search (they now break line). */
+
+/* .vs__dropdown-menu {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.vs__dropdown-option {
+  min-width: 100%;
+}
+ */
+
+  .style-chooser .vs__search::placeholder,
+  .style-chooser .vs__dropdown-toggle,
+  .style-chooser .vs__dropdown-menu {
+    background: #dfe5fb;
+    border: none;
+    color: #394066;
+    font-size: 12px;
+/*     text-transform: lowercase; */
+/*     font-variant: small-caps; */
+  }
+
+  .style-chooser .vs__clear,
+  .style-chooser .vs__open-indicator {
+    fill: #394066;
+  }
+</style>
